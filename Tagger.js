b7fb633fa8604091208ce41c9716e43186f3337b
Moein <mohammadmoein.moradi@gmail.com>
2020-10-12 19:16:04 +0330

    chore: receive DB functionalities in constructor

--- Tagger.js
+++ Tagger.js
@@ -1,13 +1,5 @@
 class Tagger {
-  constructor() {
-    const Items = [{ tags: ['100', '101', '102'], id: '1000' }];
-
-    const Tags = [
-      { root: true, childrenTags: ['101', '102'], tagName: 'Movies', items: ['1000'], id: '100' },
-      { root: false, childrenTags: [], tagName: 'Drama', items: ['1000'], id: '101' },
-      { root: false, childrenTags: [], tagName: 'American', items: ['1000'], id: '102' },
-    ];
-
+  constructor(addItemToDatabase, addTagToDatabase, getTagByID, updateTag, getRoot) {
     // handling item insertion
     this.insertItem = function (itemID = null, tagID = null) {
       // check for valid itemID and tagID
@@ -38,48 +30,6 @@ class Tagger {
       addItemToDatabase(newItem);
     };
 
-    // the function that updates properties for tag with ID of tagID
-    // this function should be provided bu user
-    // changedValues is an object of the keys and values to change e.g. {items: newItems, id: '1231'}
-    const updateTag = function (tagID = null, newValues = null) {
-      // check for valid tagID and newItems
-      if (newValues === null) throw new Error('Null newValues');
-      else if (tagID === null) throw new Error('Null TagID');
-
-      const index = Tags.findIndex((t) => t.id === tagID);
-      const newProperties = Object.keys(newValues);
-
-      for (let i = 0; i < newProperties.length; i++) {
-        const property = newProperties[i];
-        Tags[index][property] = newValues[property];
-      }
-    };
-
-    // insert new item to database
-    const addItemToDatabase = function (item) {
-      Items.push(item);
-    };
-
-    // insert new tag to database
-    const addTagToDatabase = function (tag) {
-      Tags.push(tag);
-    };
-
-    // the function that retrieves tag information using id
-    // this function should be provided by user
-    const getTagByID = function (id) {
-      return Tags.filter((tag) => tag.id === id)[0];
-    };
-
-    const getRoot = function () {
-      return Tags.filter((tag) => tag.root)[0];
-    };
-
-    function printTags() {
-      console.log(Tags);
-      console.log(Items);
-    }
-
     // handling tag insertion
     // inserting new tag, if root is true then new tag will be the parent of the current root
     this.insertTag = function (tagName = null, tagID = null, attachToID = null, root = false) {
@@ -105,10 +55,5 @@ class Tagger {
         });
       }
     };
-
-    // TESTING
-    // insertItem('1001', '100');
-    // insertTag('Cinema', '104', null, true);
-    // printTags();
   }
 }
