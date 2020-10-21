6eb374ff67dcb7d8e5ff95cb6d7f3cb209c50e58
Moein <mohammadmoein.moradi@gmail.com>
2020-10-21 18:07:25 +0330

    test: add testing delete items

--- test.js
+++ test.js
@@ -23,12 +23,24 @@ function getDocIndex(id) {
   return index;
 }
 
-const tagger = new Tagger(insertDoc, updateDoc, getDocByID, 'id', 'id');
-
 // TESTING
-tagger.createNewCluster('rt64q57scg0');
-tagger.insertTag(database[0].clusterID, 'Movies', '100', null, true);
-tagger.insertItem(database[0].clusterID, '1000', '100');
-tagger.insertTag(database[0].clusterID, 'Drama', '110', '100', false);
-console.dir(JSON.stringify(database));
-tagger.printTags(database[0].clusterID);
+async function test() {
+  const tagger = new Tagger(insertDoc, updateDoc, getDocByID, 'id', 'id');
+  tagger.createNewCluster('rt64q57scg0');
+  tagger.insertTag(database[0].clusterID, 'Movies', '100', null, true);
+  tagger.insertTag(database[0].clusterID, 'Drama', '110', '100', false);
+  tagger.insertTag(database[0].clusterID, 'Action', '105', '100', false);
+  tagger.insertTag(database[0].clusterID, 'Comedy', '103', '100', false);
+  tagger.insertTag(database[0].clusterID, 'American', '104', '103', false);
+
+  tagger.insertItem(database[0].clusterID, '1000', '100');
+  tagger.insertItem(database[0].clusterID, '1001', '103');
+  tagger.insertItem(database[0].clusterID, '1002', '105');
+
+  console.log('- - - - - - - - - - \n', JSON.stringify(database));
+  //tagger.printTags(database[0].clusterID);
+  await tagger.deleteItem(database[0].clusterID, '1001');
+  console.log('- - - - - - - - - - \n', JSON.stringify(database));
+}
+
+test();
